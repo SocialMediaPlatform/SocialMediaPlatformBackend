20799aff217a53c93d8b60516e5c7d3983c4425e
mchomans <01168876@pw.edu.pl>
2024-05-13 19:22:14 +0200

    Generate base project template

---
+++ src/main/java/com/social_media_platform/social_media_platform_backend/SocialMediaPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.social_media_platform.social_media_platform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaPlatformBackendApplication.class, args);
+	}
+
+}
