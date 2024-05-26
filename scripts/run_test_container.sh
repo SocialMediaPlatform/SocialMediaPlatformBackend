7cbb52db2d237a3332043caf66714b23ae6e2a61
Maksymilian Bilski <maksde25@gmail.com>
2024-05-26 16:48:35 +0200

    ENH: Provide shell scripts to streamline building and starting containers

---
+++ scripts/run_test_container.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Get the current directory name (repo name)
+CONTAINER=$(basename "$PWD")-test
+
+# Get the current user name and create the image name
+USER=$(whoami)
+IMAGE="${USER}/${CONTAINER}"
+
+# Ensure container is not running
+docker stop "$CONTAINER" 2> /dev/null
+
+# Run docker container
+docker run \
+    --name "$CONTAINER" \
+    --rm \
+    "$IMAGE" \
