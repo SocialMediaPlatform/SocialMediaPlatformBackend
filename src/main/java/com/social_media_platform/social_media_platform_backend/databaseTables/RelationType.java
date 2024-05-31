dd1d683212e9cf937e98f074665c58ba2a5631a3
mchomans <01168876@pw.edu.pl>
2024-05-31 19:48:22 +0200

    add relation type entity

---
+++ src/main/java/com/social_media_platform/social_media_platform_backend/databaseTables/RelationType.java
@@ -0,0 +1,11 @@
+package com.social_media_platform.social_media_platform_backend.databaseTables;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class RelationType {
+    private @Id Integer realtionId;
+    private String relationName;
+}
