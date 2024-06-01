2249d2e2300c31adda5eaba6d9b84a48d3100c92
pkutyl <01168894@pw.edu.pl>
2024-06-01 23:01:12 +0200

    CHORE: Add Conversation and ConversationMessage classes with hibernate mapping

---
+++ src/main/java/com/social_media_platform/social_media_platform_backend/databaseTables/ConversationMessage.java
@@ -0,0 +1,21 @@
+package com.social_media_platform.social_media_platform_backend.databaseTables;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class ConversationMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long messageId;
+    private String messageContent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "conversationId", nullable = false)
+    private Conversation conversation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
