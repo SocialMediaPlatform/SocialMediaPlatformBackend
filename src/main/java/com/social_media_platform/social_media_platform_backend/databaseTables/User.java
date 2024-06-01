2fdfe6ca493e37d10d4151c64c9d711996044aaf
pkutyl <01168894@pw.edu.pl>
2024-06-01 23:00:45 +0200

    CHORE: Add User class

---
+++ src/main/java/com/social_media_platform/social_media_platform_backend/databaseTables/User.java
@@ -0,0 +1,34 @@
+package com.social_media_platform.social_media_platform_backend.databaseTables;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String username;
+    private String email;
+    private String password;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<UserRelation> userRelations;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Post> posts;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<ConversationMessage> conversationMessages;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_conversation",
+            joinColumns = @JoinColumn(name = "userId"),
+            inverseJoinColumns = @JoinColumn(name = "conversationId")
+    )
+    private Set<Conversation> conversations;
+}
