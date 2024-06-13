cc5048e7581b9c704b5ae328e317dab5b1379550
mchomans <01168876@pw.edu.pl>
2024-06-13 16:02:42 +0200

    Add comment repository

---
+++ src/main/java/com/social_media_platform/social_media_platform_backend/repositiries/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.social_media_platform.social_media_platform_backend.repositiries;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.social_media_platform.social_media_platform_backend.databaseTables.Comment;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
