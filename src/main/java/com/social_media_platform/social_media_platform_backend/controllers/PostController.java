e93d726be88b922d93740f667fab5785cedc4f7e
mchomans <01168876@pw.edu.pl>
2024-06-19 19:36:05 +0200

    Fix return types

--- src/main/java/com/social_media_platform/social_media_platform_backend/controllers/PostController.java
+++ src/main/java/com/social_media_platform/social_media_platform_backend/controllers/PostController.java
@@ -1,11 +1,13 @@
 package com.social_media_platform.social_media_platform_backend.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.social_media_platform.social_media_platform_backend.controllers.responses.PostResponse;
 import com.social_media_platform.social_media_platform_backend.databaseTables.Post;
 import com.social_media_platform.social_media_platform_backend.services.PostService;
 
@@ -19,7 +21,11 @@ public class PostController {
     }
 
     @GetMapping("{userId}")
-    public ResponseEntity<List<Post>> getUserPosts(@PathVariable Long userId) {
-        return new ResponseEntity<>(postService.getUserPosts(userId), HttpStatus.OK);
+    public ResponseEntity<List<PostResponse>> getUserPosts(@PathVariable Long userId) {
+        ArrayList<PostResponse> postResponses = new ArrayList<>();
+        for (Post post : postService.getUserPosts(userId)) {
+            postResponses.add(new PostResponse(post));
+        }
+        return new ResponseEntity<>(postResponses, HttpStatus.OK);
     }
 }
