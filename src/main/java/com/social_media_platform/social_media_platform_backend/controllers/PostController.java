f106cf88516911895b0cec9cbe7fe7c7b6b78ee5
mchomans <01168876@pw.edu.pl>
2024-06-19 20:06:07 +0200

    Add posting Posts

--- src/main/java/com/social_media_platform/social_media_platform_backend/controllers/PostController.java
+++ src/main/java/com/social_media_platform/social_media_platform_backend/controllers/PostController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.social_media_platform.social_media_platform_backend.controllers.requests.AddPostRequest;
 import com.social_media_platform.social_media_platform_backend.controllers.responses.PostResponse;
 import com.social_media_platform.social_media_platform_backend.databaseTables.Post;
 import com.social_media_platform.social_media_platform_backend.services.PostService;
@@ -27,5 +28,16 @@ public class PostController {
             postResponses.add(new PostResponse(post));
         }
         return new ResponseEntity<>(postResponses, HttpStatus.OK);
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<?> createPost(@RequestBody AddPostRequest post) {
+        try {
+            postService.createPost(post.getUserId(), new Post(post.getPostDate(), post.getPostContent()));
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/com/social_media_platform/social_media_platform_backend/services/PostService.java
+++ src/main/java/com/social_media_platform/social_media_platform_backend/services/PostService.java
@@ -6,16 +6,25 @@ import org.springframework.stereotype.Service;
 
 import com.social_media_platform.social_media_platform_backend.databaseTables.Post;
 import com.social_media_platform.social_media_platform_backend.repositiries.PostRepository;
+import com.social_media_platform.social_media_platform_backend.repositiries.UserRepository;
 
 @Service
 public class PostService {
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, UserRepository userRepository) {
         this.postRepository = postRepository;
+        this.userRepository = userRepository;
     }
 
     public List<Post> getUserPosts(Long userId) {
         return postRepository.getUserPosts(userId);
+    }
+
+    public void createPost(Long userId, Post post) throws Exception{
+        post.setUser(
+                userRepository.findById(userId).orElseThrow(() -> new Exception("User not found")));
+        postRepository.save(post);
     }
 }
