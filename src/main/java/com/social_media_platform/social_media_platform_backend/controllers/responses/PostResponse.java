682850f9f2d915ac1c51bc0aa3a2e83855d73d74
mchomans <01168876@pw.edu.pl>
2024-06-19 19:35:44 +0200

    Add responses types for post and user

---
+++ src/main/java/com/social_media_platform/social_media_platform_backend/controllers/responses/PostResponse.java
@@ -0,0 +1,22 @@
+package com.social_media_platform.social_media_platform_backend.controllers.responses;
+
+import java.util.Date;
+
+import com.social_media_platform.social_media_platform_backend.databaseTables.Post;
+
+import lombok.Data;
+
+@Data
+public class PostResponse {
+    public Long postId;
+    private Date postDate;
+    private String postContent;
+    private UserResponse user;
+
+    public PostResponse(Post post) {
+        this.postId = post.getPostId();
+        this.postDate = post.getPostDate();
+        this.postContent = post.getPostContent();
+        this.user = new UserResponse(post.getUser());
+    }
+}
