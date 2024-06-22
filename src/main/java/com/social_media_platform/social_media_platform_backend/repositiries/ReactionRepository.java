1776240f4445fb8fbecd56aae2857c05709c732b
mchomans <01168876@pw.edu.pl>
2024-06-22 22:51:21 +0200

    Add getting comment reactions

--- src/main/java/com/social_media_platform/social_media_platform_backend/repositiries/ReactionRepository.java
+++ src/main/java/com/social_media_platform/social_media_platform_backend/repositiries/ReactionRepository.java
@@ -1,9 +1,18 @@
 package com.social_media_platform.social_media_platform_backend.repositiries;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.social_media_platform.social_media_platform_backend.databaseTables.Reaction;
 
 @Repository
-public interface ReactionRepository extends JpaRepository<Reaction, Long> {}
+public interface ReactionRepository extends JpaRepository<Reaction, Long> {
+    @Query("SELECT r FROM Reaction r WHERE r.post.postId = :postId")
+    List<Reaction> getPostReactions(Long postId);
+
+    @Query("SELECT r FROM Reaction r WHERE r.comment.commentId = :commentId")
+    List<Reaction> getCommReactions(Long commentId);
+}
--- src/main/java/com/social_media_platform/social_media_platform_backend/services/CommentService.java
+++ src/main/java/com/social_media_platform/social_media_platform_backend/services/CommentService.java
@@ -8,11 +8,13 @@ import org.springframework.stereotype.Service;
 
 import com.social_media_platform.social_media_platform_backend.repositiries.CommentRepository;
 import com.social_media_platform.social_media_platform_backend.repositiries.PostRepository;
+import com.social_media_platform.social_media_platform_backend.repositiries.ReactionRepository;
 import com.social_media_platform.social_media_platform_backend.repositiries.UserRepository;
 import com.social_media_platform.social_media_platform_backend.controllers.requests.AddMainCommentRequest;
 import com.social_media_platform.social_media_platform_backend.controllers.requests.AddSubCommentRequest;
 import com.social_media_platform.social_media_platform_backend.databaseTables.Comment;
 import com.social_media_platform.social_media_platform_backend.databaseTables.MainComment;
+import com.social_media_platform.social_media_platform_backend.databaseTables.Reaction;
 import com.social_media_platform.social_media_platform_backend.databaseTables.SubComment;
 
 @Service
@@ -20,14 +22,16 @@ public class CommentService {
   private final CommentRepository commentRepository;
   private final PostRepository postRepository;
   private final UserRepository userRepository;
+  private final ReactionRepository reactionRepository;
 
   public CommentService(
       CommentRepository commentRepository,
       PostRepository postRepository,
-      UserRepository userRepository) {
+      UserRepository userRepository, ReactionRepository reactionRepository) {
     this.commentRepository = commentRepository;
     this.postRepository = postRepository;
     this.userRepository = userRepository;
+    this.reactionRepository = reactionRepository;
   }
 
   public List<Comment> getCommentsInPost(Long postId) throws Exception {
@@ -45,14 +49,12 @@ public class CommentService {
   }
 
   public void addComment(AddMainCommentRequest addMainCommentRequest) throws Exception {
-    var post =
-        postRepository
-            .findById(addMainCommentRequest.getPostId())
-            .orElseThrow(() -> new Exception("Post not found"));
-    var user =
-        userRepository
-            .findById(addMainCommentRequest.getUserId())
-            .orElseThrow(() -> new Exception("User not found"));
+    var post = postRepository
+        .findById(addMainCommentRequest.getPostId())
+        .orElseThrow(() -> new Exception("Post not found"));
+    var user = userRepository
+        .findById(addMainCommentRequest.getUserId())
+        .orElseThrow(() -> new Exception("User not found"));
 
     var mainComment = new MainComment();
     mainComment.setCommentContents(addMainCommentRequest.getCommentContents());
@@ -64,17 +66,15 @@ public class CommentService {
   }
 
   public void addComment(AddSubCommentRequest addMainCommentRequest) throws Exception {
-    var mainComment =
-        commentRepository
-            .findById(addMainCommentRequest.getMainCommentId())
-            .orElseThrow(() -> new Exception("Main comment not found"));
+    var mainComment = commentRepository
+        .findById(addMainCommentRequest.getMainCommentId())
+        .orElseThrow(() -> new Exception("Main comment not found"));
     if (!(mainComment instanceof MainComment)) {
       throw new Exception("Main comment not found");
     }
-    var user =
-        userRepository
-            .findById(addMainCommentRequest.getUserId())
-            .orElseThrow(() -> new Exception("User not found"));
+    var user = userRepository
+        .findById(addMainCommentRequest.getUserId())
+        .orElseThrow(() -> new Exception("User not found"));
 
     var subComment = new SubComment();
     subComment.setCommentContents(addMainCommentRequest.getCommentContents());
@@ -83,5 +83,10 @@ public class CommentService {
     subComment.setUser(user);
 
     commentRepository.save(subComment);
+  }
+
+  public List<Reaction> getCommentReactions(Long commentId) throws Exception {
+    commentRepository.findById(commentId).orElseThrow(() -> new Exception("Comment not found"));
+    return reactionRepository.getCommReactions(commentId);
   }
 }
