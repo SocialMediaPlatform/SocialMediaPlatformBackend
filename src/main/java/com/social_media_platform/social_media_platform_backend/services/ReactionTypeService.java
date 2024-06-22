229dd246bff3a4beb0a92edb139d67654fc76e15
mchomans <01168876@pw.edu.pl>
2024-06-22 10:28:36 +0200

    Add reaction type endpoint for getting all of types

---
+++ src/main/java/com/social_media_platform/social_media_platform_backend/services/ReactionTypeService.java
@@ -0,0 +1,21 @@
+package com.social_media_platform.social_media_platform_backend.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.social_media_platform.social_media_platform_backend.repositiries.ReactionTypeRepository;
+import com.social_media_platform.social_media_platform_backend.databaseTables.ReactionType;
+
+@Service
+public class ReactionTypeService {
+    private ReactionTypeRepository reactionTypeRepository;
+
+    public ReactionTypeService(ReactionTypeRepository reactionTypeRepository) {
+        this.reactionTypeRepository = reactionTypeRepository;
+    }
+
+    public List<ReactionType> getAllReactionTypes() {
+        return reactionTypeRepository.findAll();
+    }
+}
