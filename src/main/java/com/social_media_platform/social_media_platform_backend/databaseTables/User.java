7a52c912a9a9d887f132a93e94dac2307b07e074
pkutyl <01168894@pw.edu.pl>
2024-06-22 18:50:15 +0200

    REFACTOR: Convert user to implement UserDetails

--- src/main/java/com/social_media_platform/social_media_platform_backend/databaseTables/User.java
+++ src/main/java/com/social_media_platform/social_media_platform_backend/databaseTables/User.java
@@ -1,14 +1,23 @@
 package com.social_media_platform.social_media_platform_backend.databaseTables;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Set;
 
 @Data
 @Entity
 @Table(name = "users")
-public class User {
+@NoArgsConstructor
+public class User implements UserDetails {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long userId;
@@ -32,4 +41,46 @@ public class User {
       joinColumns = @JoinColumn(name = "userId"),
       inverseJoinColumns = @JoinColumn(name = "conversationId"))
   private Set<Conversation> conversations;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Collections.emptyList();
+  }
+
+  @Override
+  @JsonIgnore
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
