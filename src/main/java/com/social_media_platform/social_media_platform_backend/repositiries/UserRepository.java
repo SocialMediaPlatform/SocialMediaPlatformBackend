4dcc62c55c28613a18a98e5409950d0acbd6f8df
pkutyl <01168894@pw.edu.pl>
2024-06-22 18:50:57 +0200

    FEAT: Add repository method for email search

--- src/main/java/com/social_media_platform/social_media_platform_backend/repositiries/UserRepository.java
+++ src/main/java/com/social_media_platform/social_media_platform_backend/repositiries/UserRepository.java
@@ -5,5 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import com.social_media_platform.social_media_platform_backend.databaseTables.User;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
+
