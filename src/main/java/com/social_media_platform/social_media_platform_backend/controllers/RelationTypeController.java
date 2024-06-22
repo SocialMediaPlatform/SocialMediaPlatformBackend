aac26def70e7c4a2c82955260b38055a383012f0
mchomans <01168876@pw.edu.pl>
2024-06-22 10:37:42 +0200

    Add realtion type endpoint

---
+++ src/main/java/com/social_media_platform/social_media_platform_backend/controllers/RelationTypeController.java
@@ -0,0 +1,27 @@
+package com.social_media_platform.social_media_platform_backend.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.social_media_platform.social_media_platform_backend.databaseTables.RelationType;
+import com.social_media_platform.social_media_platform_backend.services.RelationTypeService;
+
+@RestController
+@RequestMapping("/api/v1/reactionType")
+public class RelationTypeController {
+    private RelationTypeService relationTypeService;
+
+    public RelationTypeController(RelationTypeService relationTypeService) {
+        this.relationTypeService = relationTypeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RelationType>> getAllRealtionTypes() {
+        return new ResponseEntity<>(relationTypeService.getAllRelationTypes(), HttpStatus.OK);
+    }
+}
