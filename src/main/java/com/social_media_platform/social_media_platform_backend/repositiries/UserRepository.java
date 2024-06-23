a3f9049e26fac0939d340ff3a8d6e9dfb1612b2b
pkutyl <01168894@pw.edu.pl>
2024-06-24 00:15:07 +0200

    FEAT: Add UserRepository

--- src/main/java/com/social_media_platform/social_media_platform_backend/repositiries/UserRepository.java
+++ src/main/java/com/social_media_platform/social_media_platform_backend/repositiries/UserRepository.java
@@ -3,6 +3,7 @@ package com.social_media_platform.social_media_platform_backend.repositiries;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.social_media_platform.social_media_platform_backend.databaseTables.User;
@@ -10,7 +11,7 @@ import com.social_media_platform.social_media_platform_backend.databaseTables.Us
 import jakarta.transaction.Transactional;
 
 import java.util.Optional;
-
+import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByEmail(String email);
@@ -26,4 +27,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
   @Transactional
   @Query("update User u set u.password = :newPassword where u.userId = :userId")
   void setPassword(Long userId, String newPassword);
+
+  @Query("select u from User u full join u.userRelations ur full join ur.relationType rt where u.username like %:username% and (rt.relationTypeName is null or rt.relationTypeName <> 'blocked') and u.userId <> :userId")
+  List<User> findUsersByUsernamePattern(@Param("username") String username, @Param("userId") Long userId);
 }
