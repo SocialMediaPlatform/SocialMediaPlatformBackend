9ce51f1f6c01480f73dd1bd18703d69c23a26726
Maksymilian Bilski <maksde25@gmail.com>
2024-06-24 11:34:05 +0200

    FEAT: Implement ConversationRequest class

---
+++ src/main/java/com/social_media_platform/social_media_platform_backend/controllers/requests/CreateConversationRequest.java
@@ -0,0 +1,13 @@
+package com.social_media_platform.social_media_platform_backend.controllers.requests;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class CreateConversationRequest {
+    private List<Long> recipientUserIds;
+    private String messageContent;
+    private Date messageDate;
+}
