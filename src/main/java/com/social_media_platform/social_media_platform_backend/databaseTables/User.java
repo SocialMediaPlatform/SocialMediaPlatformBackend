7997c7e1934599f7989262d4efae7ef13fd0fc2f
Maksymilian Bilski <maksde25@gmail.com>
2024-06-26 00:03:29 +0200

    FIX: Implement toString method in User class to prevent recursive calls

--- src/main/java/com/social_media_platform/social_media_platform_backend/databaseTables/User.java
+++ src/main/java/com/social_media_platform/social_media_platform_backend/databaseTables/User.java
@@ -87,4 +87,13 @@ public class User implements UserDetails {
   public boolean isEnabled() {
     return true;
   }
+
+  @Override
+  public String toString() {
+    return "User{" +
+            "userId=" + userId +
+            ", username='" + username + '\'' +
+            ", email='" + email + '\'' + 
+            '}';
+  }
 }
