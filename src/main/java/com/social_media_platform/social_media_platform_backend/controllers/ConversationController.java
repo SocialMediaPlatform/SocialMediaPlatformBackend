cc9db0c21374ed9a6f50b0e1949bf7c5a43a8e95
Maksymilian Bilski <maksde25@gmail.com>
2024-06-25 15:18:24 +0200

    FEAT: Implement GetConversationWithUserResponse, getConversationMessages and getUserGroupConversations controller methods

--- src/main/java/com/social_media_platform/social_media_platform_backend/controllers/ConversationController.java
+++ src/main/java/com/social_media_platform/social_media_platform_backend/controllers/ConversationController.java
@@ -1,17 +1,22 @@
 package com.social_media_platform.social_media_platform_backend.controllers;
 
+import com.social_media_platform.social_media_platform_backend.Helpers.GroupConversationInfo;
 import com.social_media_platform.social_media_platform_backend.controllers.responses.ConversationResponse;
+import com.social_media_platform.social_media_platform_backend.controllers.responses.GetConversationWithUserResponse;
 import com.social_media_platform.social_media_platform_backend.databaseTables.Conversation;
 import com.social_media_platform.social_media_platform_backend.services.ConversationService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.social_media_platform.social_media_platform_backend.controllers.requests.CreateConversationRequest;
+import com.social_media_platform.social_media_platform_backend.databaseTables.ConversationMessage;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.List;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/v1/conversations")
@@ -19,22 +24,62 @@ public class ConversationController {
 
     private final ConversationService conversationService;
 
-    @Autowired
     public ConversationController(ConversationService conversationService) {
         this.conversationService = conversationService;
     }
 
     @PostMapping("create")
     public ResponseEntity<ConversationResponse> createConversation(@RequestBody CreateConversationRequest createConversationRequest) {
-        System.out.println("Endpoint was accessed");
+        UserDetails currenUserDetails = getCurrentUserDetails();
+        Conversation createdConversation = conversationService.createConversation(createConversationRequest, currenUserDetails);
+        return new ResponseEntity<>(new ConversationResponse(createdConversation), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{userId}/conversation")
+    public ResponseEntity<GetConversationWithUserResponse> getConversationWithUser(
+            @PathVariable Long userId,
+            @RequestParam(required = false, defaultValue = "0") Integer start,
+            @RequestParam(required = false, defaultValue = "20") Integer limit) {
+
+        UserDetails currenUserDetails = getCurrentUserDetails();
+        Long conversationId = conversationService.getConversationsIdWithUser(userId, currenUserDetails);
+
+        if (conversationId == null) {
+            return new ResponseEntity<>(new GetConversationWithUserResponse(null, new ArrayList<>()), HttpStatus.OK);
+        }
+
+        List<ConversationMessage> conversationMessages = conversationService.getConversationMessages(conversationId, start, limit);
+        return new ResponseEntity<>(new GetConversationWithUserResponse(conversationId, conversationMessages), HttpStatus.OK);
+    }
+
+    @GetMapping("/{conversationId}/messages")
+    public ResponseEntity<List<ConversationMessage>> getConversationMessages(
+            @PathVariable Long conversationId,
+            @RequestParam(required = false, defaultValue = "0") Integer start,
+            @RequestParam(required = false, defaultValue = "20") Integer limit) {
+        UserDetails currentUserDetails = getCurrentUserDetails();
+        if (!conversationService.isUserInConversation(conversationId, currentUserDetails)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        List<ConversationMessage> messages = conversationService.getConversationMessages(conversationId, start, limit);
+        return new ResponseEntity<>(messages, HttpStatus.OK);
+    }
+
+    @GetMapping("/group-conversations")
+    public ResponseEntity<List<GroupConversationInfo>> getUserGroupConversations() {
+        UserDetails currentUserDetails = getCurrentUserDetails();
+        List<GroupConversationInfo> groupConversationInfo = conversationService.getGroupConversationDetails(currentUserDetails);
+
+        if (groupConversationInfo.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(groupConversationInfo, HttpStatus.OK);
+    }
+
+    private UserDetails getCurrentUserDetails() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Object principal = authentication.getPrincipal();
-        System.out.println("Principal type: " + principal.getClass().getName());
-        System.out.println("Principal value: " + principal.toString());
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-
-        Conversation createdConversation = conversationService.createConversation(createConversationRequest, userDetails);
-
-        return new ResponseEntity<>(new ConversationResponse(createdConversation), HttpStatus.CREATED);
+        return (UserDetails) principal;
     }
 }
