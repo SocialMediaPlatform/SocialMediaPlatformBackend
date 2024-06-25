3734407ae10e45b7c7061bef2e9837d6015bee97
Maksymilian Bilski <maksde25@gmail.com>
2024-06-25 15:22:38 +0200

    FIX: Change column names in @JoinTable adnotation for conversations member field so they reflect real ones

--- src/main/java/com/social_media_platform/social_media_platform_backend/databaseTables/User.java
+++ src/main/java/com/social_media_platform/social_media_platform_backend/databaseTables/User.java
@@ -3,6 +3,7 @@ package com.social_media_platform.social_media_platform_backend.databaseTables;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,9 +11,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Set;
+import java.util.HashSet;
 
 @Data
 @Entity
+@EqualsAndHashCode(exclude = {"conversationMessages", "conversations"})
 @Table(name = "users")
 @NoArgsConstructor
 public class User implements UserDetails {
@@ -31,17 +34,18 @@ public class User implements UserDetails {
   private Set<Post> posts;
 
   @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-  private Set<ConversationMessage> conversationMessages;
-
-  @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
   private Set<Reaction> reactions;
 
-  @ManyToMany(fetch = FetchType.LAZY)
+  @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  private Set<ConversationMessage> conversationMessages;
+
+  @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.ALL})
   @JoinTable(
-      name = "user_conversation",
-      joinColumns = @JoinColumn(name = "userId"),
-      inverseJoinColumns = @JoinColumn(name = "conversationId"))
-  private Set<Conversation> conversations;
+          name = "user_conversation",
+          joinColumns = @JoinColumn(name = "user_id"),
+          inverseJoinColumns = @JoinColumn(name = "conversation_id")
+  )
+  private Set<Conversation> conversations = new HashSet<>();
 
   public User(String username, String email, String password) {
     this.username = username;
